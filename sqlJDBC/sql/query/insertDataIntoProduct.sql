bb0b4b7201d677069f7b406ccc3a8643548c0f55
Andrey Semenov <Andrey4281@yandex.ru>
2019-09-07 20:59:43 +0500

    Фильтры[#161693]

---
+++ sqlJDBC/sql/query/insertDataIntoProduct.sql
@@ -0,0 +1,31 @@
+﻿INSERT INTO type(name)
+VALUES
+('сыр'),
+('молоко'),
+('мороженное');
+
+INSERT INTO product(name,type_id,expired_date, price)
+VALUES
+('сыр голландский',(SELECT type.id FROM type WHERE type.name='сыр'),'2019-09-21 00:00',700),
+('сыр российский',(SELECT type.id FROM type WHERE type.name='сыр'),'2019-10-21 00:00',400),
+('мороженное пломбир', (SELECT type.id FROM type WHERE type.name='мороженное'),'2019-09-25 00:00',50),
+('шоколадное мороженное', (SELECT type.id FROM type WHERE type.name='мороженное'),'2019-10-10 00:00',70),
+('молоко ирбитское', (SELECT type.id FROM type WHERE type.name='молоко'),'2019-09-10 00:00',100),
+('молоко простоквашино', (SELECT type.id FROM type WHERE type.name='молоко'),'2019-10-01 00:00',120);
+
+INSERT INTO type(name)
+VALUES
+('хлеб');
+
+INSERT INTO product(name,type_id,expired_date, price)
+VALUES
+('батон',(SELECT type.id FROM type WHERE type.name='хлеб'),'2019-09-21 00:00',700),
+('булочка',(SELECT type.id FROM type WHERE type.name='хлеб'),'2019-10-21 00:00',400),
+('пряники', (SELECT type.id FROM type WHERE type.name='хлеб'),'2019-09-25 00:00',50),
+('белый хлеб', (SELECT type.id FROM type WHERE type.name='хлеб'),'2019-10-10 00:00',70),
+('ржаной хлеб', (SELECT type.id FROM type WHERE type.name='хлеб'),'2019-09-10 00:00',100),
+('каральки', (SELECT type.id FROM type WHERE type.name='хлеб'),'2019-10-01 00:00',120),
+('баранки', (SELECT type.id FROM type WHERE type.name='хлеб'),'2019-10-01 00:00',120),
+('сушки', (SELECT type.id FROM type WHERE type.name='хлеб'),'2019-10-01 00:00',120),
+('печенье', (SELECT type.id FROM type WHERE type.name='хлеб'),'2019-10-01 00:00',120),
+('торт', (SELECT type.id FROM type WHERE type.name='хлеб'),'2019-10-01 00:00',120);
---
+++ sqlJDBC/sql/query/queryToProduct.sql
@@ -0,0 +1,41 @@
+﻿--1
+SELECT product.id,product.name,product.expired_date,product.price FROM product
+INNER JOIN type ON product.type_id = type.id
+WHERE type.name='сыр';
+
+--2
+SELECT product.id,product.name,product.expired_date,product.price FROM product
+WHERE product.name like '%мороженное%';
+
+--3
+SELECT name, expired_date FROM product
+WHERE (date_trunc('month', CURRENT_DATE + INTERVAL '1 month'), date_trunc('month', CURRENT_DATE + INTERVAL '2 month'))
+OVERLAPS (expired_date, expired_date);
+
+--4
+SELECT product.id,product.name,product.expired_date,product.price FROM product
+WHERE product.price = (SELECT MAX(price) FROM product);
+
+--5
+SELECT type.name, COUNT(product.id)
+FROM type INNER JOIN product ON type.id=product.type_id
+GROUP BY (type.name);
+
+SELECT COUNT(product.id)
+FROM product INNER JOIN type ON product.type_id=type.id
+WHERE type.name='сыр';
+
+--6
+SELECT product.id,product.name,product.expired_date,product.price FROM product
+INNER JOIN type ON product.type_id=type.id
+WHERE type.name='сыр' OR type.name='молоко';
+
+--7
+SELECT type.name, COUNT(type.name) AS count
+FROM type INNER JOIN product ON type.id=product.type_id
+GROUP BY type.name
+HAVING COUNT(type.name)<10;
+
+--8
+SELECT product.id,product.name,product.expired_date,product.price,type.name FROM product
+INNER JOIN type ON product.type_id=type.id;
