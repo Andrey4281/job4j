ae04978198e9a1b41de0656660a5e0e3294f6477
Andrey Semenov <Andrey4281@yandex.ru>
2019-09-08 21:13:38 +0500

    Хранилище машин[#161696]

---
+++ sqlJDBC/sql/outerJoin/createTableForCarCatalog.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE carcass (
+id SERIAL PRIMARY KEY,
+name VARCHAR(100)
+);
+
+CREATE TABLE engine (
+id SERIAL PRIMARY KEY,
+name VARCHAR(100)
+);
+
+CREATE TABLE transmission (
+id SERIAL PRIMARY KEY,
+name VARCHAR(100)
+);
+
+CREATE TABLE cars (
+id SERIAL PRIMARY KEY,
+name VARCHAR(100),
+id_carcass INTEGER REFERENCES carcass(id),
+id_engine INTEGER REFERENCES engine(id),
+id_transmission INTEGER REFERENCES transmission(id)
+);
---
+++ sqlJDBC/sql/outerJoin/insertDataIntoCarCatalog.sql
@@ -0,0 +1,26 @@
+﻿INSERT INTO engine(name)
+VALUES
+('engineOne'),
+('engineTwo'),
+('engineThree');
+
+INSERT INTO carcass(name)
+VALUES
+('carcassOne'),
+('carcassTwo'),
+('carcassThree');
+
+INSERT INTO transmission(name)
+VALUES
+('transmissionOne'),
+('transmissionTwo'),
+('transmissionThree');
+
+INSERT INTO cars(name, id_carcass, id_engine, id_transmission)
+VALUES
+('carOne',(SELECT id FROM carcass WHERE carcass.name='carcassOne'),
+(SELECT id FROM engine WHERE engine.name='engineOne'),
+(SELECT id FROM transmission WHERE transmission.name='transmissionOne')),
+('carTwo',(SELECT id FROM carcass WHERE carcass.name='carcassTwo'),
+(SELECT id FROM engine WHERE engine.name='engineTwo'),
+(SELECT id FROM transmission WHERE transmission.name='transmissionTwo'));
