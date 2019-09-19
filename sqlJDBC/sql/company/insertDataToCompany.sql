3dad46337219ee29a2cc38e8210d07ea6aaa5f47
Andrey Semenov <Andrey4281@yandex.ru>
2019-09-19 19:56:55 +0500

    SQL Select by company[#161701]

---
+++ sqlJDBC/sql/company/insertDataToCompany.sql
@@ -0,0 +1,16 @@
+﻿INSERT INTO company(name)
+VALUES
+('kontur'),
+('naumen'),
+('pro-soft'),
+('yandex'),
+('mail.ru');
+
+INSERT INTO person(name, company_id)
+VALUES
+('Andrey', (SELECT company.id FROM company WHERE company.name = 'naumen')),
+('Sergey', (SELECT company.id FROM company WHERE company.name = 'naumen')),
+('Alexey', (SELECT company.id FROM company WHERE company.name = 'kontur')),
+('Roman', (SELECT company.id FROM company WHERE company.name = 'pro-soft')),
+('Petr', (SELECT company.id FROM company WHERE company.name = 'yandex')),
+('Vitaliy', (SELECT company.id FROM company WHERE company.name = 'mail.ru'));
---
+++ sqlJDBC/sql/company/queryToCompany.sql
@@ -0,0 +1,12 @@
+﻿--1
+SELECT person.name, company.name FROM person
+INNER JOIN company ON person.company_id = company.id
+WHERE company.id != 5;
+
+--2
+SELECT company.name, COUNT(person.id) AS countOfPerson FROM company
+INNER JOIN person ON company.id = person.company_id 
+GROUP BY company.name
+HAVING COUNT(person.id) = (SELECT MAX(t.cou) FROM (SELECT COUNT(person.id) AS cou FROM
+person INNER JOIN company ON person.company_id = company.id
+GROUP BY company.id) AS t);
