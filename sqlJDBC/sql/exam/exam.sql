40c9e761d291464dcb0f435a975f969ea05d284c
Andrey Semenov <Andrey4281@yandex.ru>
2019-09-28 14:38:04 +0500

    Удалить дубликаты через SQL [#106936]

---
+++ sqlJDBC/sql/exam/exam.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE IF NOT EXISTS cities (
+id SERIAL PRIMARY KEY,
+name VARCHAR(50)
+);
+
+INSERT INTO cities (name)
+VALUES
+('Москва'),
+('Москва'),
+('СПБ'),
+('Казань'),
+('Казань'),
+('Новгород');
+
+CREATE INDEX name_index ON cities(name);
+
+ALTER TABLE cities
+ADD id_city SERIAL;
+
+DELETE FROM cities
+WHERE cities.id_city > (SELECT MIN(t.id_city)
+FROM cities AS t WHERE t.name=cities.name);
+
+ALTER TABLE cities
+DROP COLUMN id_city;
