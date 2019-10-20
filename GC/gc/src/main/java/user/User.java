e0e71bfcd62d43196481e940942eeee0e2c99ad1
Andrey Semenov <Andrey4281@yandex.ru>
2019-10-20 11:20:26 +0500

    Демонстрация работы GC[#161785]

---
+++ GC/gc/src/main/java/user/User.java
@@ -0,0 +1,27 @@
+package user;
+
+import com.sun.management.VMOption;
+
+public class User {
+    private final String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("finalize");
+    }
+
+    public static void main(String[] args) {
+        int mb = 1024 * 1024;
+        Runtime runtime = Runtime.getRuntime();
+        for (int i = 0 ; i <10000; i++) {
+            System.out.println("Used memory " + (runtime.totalMemory() - runtime.freeMemory()) / mb);
+            User user = new User("a");
+            user = null;
+        }
+    }
+}
