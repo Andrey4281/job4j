cbd5c535853e4c3789f5bd6a2b7fa33e76580380
Andrey Semenov <Andrey4281@yandex.ru>
2019-10-29 11:03:53 +0500

    1. Пин-понг.[#191183]

---
+++ Multithreading/multithreading/src/main/java/pingpong/RectangleMove.java
@@ -0,0 +1,30 @@
+package pingpong;
+
+import javafx.scene.shape.Rectangle;
+
+public class RectangleMove implements Runnable {
+    private final Rectangle rect;
+
+    public RectangleMove(Rectangle rect) {
+        this.rect = rect;
+    }
+
+    @Override
+    public void run() {
+        int moving = 1;
+        while (true) {
+            if (this.rect.getX() >= 300 - this.rect.getWidth()) {
+                moving = -1;
+            } else if (this.rect.getX() <= 0) {
+                moving = +1;
+            }
+
+            this.rect.setX(this.rect.getX() + moving);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
