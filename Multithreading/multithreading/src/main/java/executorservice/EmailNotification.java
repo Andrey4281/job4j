a79fba87990b3a69debd1b51a21cf73e6e27059c
Andrey Semenov <Andrey4281@yandex.ru>
2019-11-07 19:25:57 +0500

    2. ExecutorService рассылка почты.[#191173]

---
+++ Multithreading/multithreading/src/main/java/executorservice/EmailNotification.java
@@ -0,0 +1,31 @@
+package executorservice;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        pool.submit(()->{
+            String subject = String.format("Notification %s to email %s", user.getUserName(), user.getEmail());
+            String body = String.format("Add a new event to %s", user.getUserName());
+            send(subject, body, user.getEmail());
+        });
+    }
+
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+}
---
+++ Multithreading/multithreading/src/main/java/executorservice/User.java
@@ -0,0 +1,19 @@
+package executorservice;
+
+public class User {
+     private final String userName;
+     private final String email;
+
+    public User(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+}
